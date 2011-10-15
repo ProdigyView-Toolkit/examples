1aac9a2eb2b7382bbb79e2d171ef37a8078ef9d2
ProdigyView <devindixon@BlackMage.local>
2011-10-15 18:44:29 -0400

    Add observer example

---
+++ examples/data/Observers.php
@@ -0,0 +1,83 @@
+<?php
+//Include the DEFINES and boo the system
+include_once('../../DEFINES.php');
+require_once(PV_CORE.'_BootCompleteSystem.php');
+
+class StalkerBook {
+	
+	public function updateFeed($message) {
+		echo '<p>Someone is stalking you!: '.$message.'</p>';
+	}
+	
+}//end class
+
+class ClutterSpace {
+	
+	public static function updateWall($message) {
+		echo '<p>Someone is adding clutter to your wall!: '.$message.'</p>';
+	}
+}
+
+//Have messenger extend PVObject
+class Messenger extends PVObject {
+	
+	public function addMessage($message){
+		
+		echo '<p>You have a new message: '.$message.'</p>';
+		//Notifies that an event has occurred
+		$this->_notify('new_message', $message);
+	}
+}
+
+echo '<h1>Instance Observer</h1>';
+
+$messenger = new Messenger();
+$messenger->addMessage('One Framework to rule them all!');
+
+echo '<h2>Add observers</h2>';
+
+//Add Event that will call an instance of an object
+$messenger->_addObserver('new_message' , 'StalkerBook', 'updateFeed', $options=array('object'=>'instance'));
+//Add Event that will call a static method
+$messenger->_addObserver('new_message' , 'ClutterSpace', 'updateWall');
+$messenger->addMessage('What was that wood chuck rhyme again?');
+
+
+echo '<h1>Static Observer</h1>';
+
+
+class FiveSquare {
+	
+	public function updateFeed($message) {
+		echo '<p>Someone is bumping you!: '.$message.'</p>';
+	}
+	
+}//end class
+
+class PointlessIN {
+	
+	public static function updateWall($message) {
+		echo '<p>Someone jobless has sent you a message: '.$message.'</p>';
+	}
+}
+
+//Have PChat extend PVStatic Object
+class PChat extends PVStaticObject {
+	
+	public function addMessage($message){
+		
+		echo '<p>PChat is notifying you: '.$message.'</p>';
+		//Notifies that an event has occurred
+		self::_notify('new_message', $message);
+	}
+}
+
+PChat::addMessage('One Framework to rule them all!');
+
+echo '<h2>Add observers</h2>';
+
+//Add Event that will call an instance of an object
+PChat::_addObserver('new_message' , 'FiveSquare', 'updateFeed', $options=array('object'=>'instance'));
+//Add Event that will call a static method
+PChat::_addObserver('new_message' , 'PointlessIN', 'updateWall');
+PChat::addMessage('What was that wood chuck rhyme again?');
