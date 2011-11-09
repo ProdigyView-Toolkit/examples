07637f519e350978cc6aea32f25ca14a4fb9a5ae
ProdigyView <devindixon@BlackMage.local>
2011-11-09 11:06:11 -0500

    Removed examplesff

---
+++ resources/libraries/pv_switch/source/PowerPlanet.php
@@ -0,0 +1,13 @@
+<?php
+class PowerPlant {
+		
+	public static function on(){
+		echo '<p>Turn The  Power Plant On</p>';
+	}
+	
+	public static function off(){
+		echo '<p>Turn The Power Plant Off</p>';
+	}
+	
+}
+?>
