52f3b9ba3871f252de24564a1e897c04276a370d
ProdigyView <devindixon@BlackMage.local>
2011-12-28 18:17:24 -0500

    Updated conversion process for video

--- examples/avi/Video_Conversion.php
+++ examples/avi/Video_Conversion.php
@@ -18,17 +18,41 @@ $video_file = PV_ROOT.DS.'examples'.DS.'content'.DS.'example_files'.DS.'sample_v
 PVVideo::convertVideoFile($video_file , PV_ROOT.PV_VIDEO.'video_1.mp4');
 
 
-$output_options = array(
+$options = array(
 	'output_vcodec' => 'libtheora'
 );
 //Convert the video to new format and set save location
-PVVideo::convertVideoFile($video_file , PV_ROOT.PV_VIDEO.'video_2.ogv');
+PVVideo::convertVideoFile($video_file , PV_ROOT.PV_VIDEO.'video_2.ogv', $options);
+
+$options = array(
+	'output_ss' => '00:0:20',
+);
+//Convert the video to new format and have the video start 20 seconds late
+PVVideo::convertVideoFile($video_file , PV_ROOT.PV_VIDEO.'video_2.avi', $options);
+
+$options = array(
+	'output_ss' => '00:0:30',
+	'output_t' => '30'
+);
+//Convert the video to new format and have the video start 30 seconds late
+//run for 30 secods
+PVVideo::convertVideoFile($video_file , PV_ROOT.PV_VIDEO.'video_3.avi', $options);
 
 
-$output_options = array(
+$options = array(
 	'output_r' => '1',
-	'output_s'=> '4cif',
+	'output_vframes'=> '20',
 	'output_f' => 'image2'
 );
-//Convert the video to new format and set save location
-PVVideo::convertVideoFile($video_file , PV_ROOT.PV_VIDEO.'image-%3d.jpeg');
+//Convert the video to an image and only output 20 images
+PVVideo::convertVideoFile($video_file , PV_ROOT.PV_VIDEO.'image-%3d.jpeg', $options);
+
+$options = array(
+	'output_r' => '1',
+	'output_vframes'=> '10',
+	'output_f' => 'image2',
+	'output_ss' => '00:02:15',
+	'output_t' => '20'
+);
+//Convert the video to an image, start at 2 minutes, go for 2- 10 seconds
+PVVideo::convertVideoFile($video_file , PV_ROOT.PV_VIDEO.'image2-%3d.jpeg', $options);
