30c0d1425b949303dc30f0f8e4d057cdf2bbbdc2
ProdigyView <devindixon@blackmage.home>
2011-12-29 10:50:24 -0500

    Adding audio examples

---
+++ examples/avi/Audio_Conversion.php
@@ -0,0 +1,51 @@
+<?php
+//Turn on error reporting
+ini_set('display_errors','On');
+error_reporting(E_ALL); 
+
+//Only the class loader is needed because no database connection
+//or supporting libraries are needed
+include_once ('../../DEFINES.php');
+require_once (PV_CORE . '_classLoader.php');
+
+//Set the location of ffmpeg on your server
+$config = array('converter' => 'ffmpeg');
+PVAudio::init($config);
+
+$audio_file = PV_ROOT.DS.'examples'.DS.'content'.DS.'example_files'.DS.'sample_audio.mp3';
+
+PVAudio::convertAudioFile($audio_file, PV_ROOT.PV_AUDIO.'sampe_audio_1.wav');
+
+
+$options = array(
+	'output_an' => true
+);
+//Disable the audio of the file
+PVAudio::convertAudioFile($audio_file, PV_ROOT.PV_AUDIO.'sampe_audio_2.mp3', $options);
+
+$options = array(
+	'output_acodec' => 'libvorbis'
+);
+//Convert to sample_audio using a code
+PVAudio::convertAudioFile($audio_file, PV_ROOT.PV_AUDIO.'sampe_audio_2.ogg', $options);
+
+$options = array(
+	'output_ab' => '160k'
+);
+//Lower the bit rate to 160k
+PVAudio::convertAudioFile($audio_file, PV_ROOT.PV_AUDIO.'compressed_1.mp3', $options);
+
+$options = array(
+	'output_ab' => '128k',
+	'output_ss' => '00:00:30'
+);
+//Lower the bit rate to 128k and start coding
+PVAudio::convertAudioFile($audio_file, PV_ROOT.PV_AUDIO.'compressed_2.mp3', $options);
+
+$options = array(
+	'output_ab' => '96k',
+	'output_ss' => '00:00:20',
+	'output_t' => '25'
+);
+//Lower the bit rate to 96k, start at 20 seconds and convert only 25 seconds
+PVAudio::convertAudioFile($audio_file, PV_ROOT.PV_AUDIO.'compressed_3.mp3', $options);
--- examples/avi/FFMPEG_Audio.php
+++ examples/avi/FFMPEG_Audio.php
@@ -9,14 +9,21 @@ include_once ('../../DEFINES.php');
 require_once (PV_CORE . '_classLoader.php');
 
 $audio_file = PV_ROOT.DS.'examples'.DS.'content'.DS.'example_files'.DS.'sample_audio.mp3';
-echo $audio_file.'<br />';
-echo PV_ROOT.PV_AUDIO.'sample1.mp4';
 
 //Path To your FFMPEG
 $ffmpeg = 'ffmpeg';
 
 //Convert the file to the same format with different name
-exec($ffmpeg.' -i '.$audio_lfile.' '.PV_ROOT.PV_AUDIO.'audio_sample.wav');
+exec($ffmpeg.' -i '.$audio_file.' '.PV_ROOT.PV_AUDIO.'audio_sample.mp3');
+
+//Convert the file to a different format
+exec($ffmpeg.' -i '.$audio_file.' '.PV_ROOT.PV_AUDIO.'audio_sample_1.wav');
 
 //Convert the file to the same format with different name
-exec($ffmpeg.' -i '.$audio_lfile.' -b 128k '.PV_ROOT.PV_AUDIO.'audio_sample.wav');
+exec($ffmpeg.' -i '.$audio_file.' -b 128k '.PV_ROOT.PV_AUDIO.'audio_sample_2.wav');
+
+//Record the sound coming in from a device and save it to an mp3
+exec($ffmpeg.' -f oss -i /dev/dsp '.PV_ROOT.PV_AUDIO.'recorded_sound.mp3');
+
+//Convert a text file into sound and listen to it
+exec($ffmpeg.' -i cat .'.PV_ROOT.'README.txt > /dev/dsp '.PV_ROOT.PV_AUDIO.'recorded_file_sound.mp3');
